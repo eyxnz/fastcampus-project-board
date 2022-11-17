98c23efdef8f9e18497946b514b229e025176f97
eyxnz <todaytubuck@naver.com>
2022-11-17 15:32:32 +0900

    #13 - 게시판 뷰 기본 골격 작성
    컨트롤러 핸들러 메소드로 테스트를 통과하게 열어주었다.
    적절한 뷰 뼈대를 작성하였다.
    테스트에서는 media type 검사에서 부가적인 옵션이 들어갈 수 있음을 확인했기 때문에 적절히 테스트 메소드를 수정하였다.

--- src/main/java/com/fastcampus/board/controller/ArticleController.java
+++ src/main/java/com/fastcampus/board/controller/ArticleController.java
@@ -1,9 +1,20 @@
 package com.fastcampus.board.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequestMapping("/articles")
 @Controller
 public class ArticleController { // 게시글
+    // 게시글 리스트 (게시판) 페이지
+    @GetMapping
+    public String articles(ModelMap map) {
+        map.addAttribute("articles", List.of());
+
+        return "articles/index.html";
+    }
 }
