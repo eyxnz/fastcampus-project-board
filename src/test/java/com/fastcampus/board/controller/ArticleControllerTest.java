147ae1034291d505953bda5850b2bbfed88bf014
eyxnz <todaytubuck@naver.com>
2022-11-17 16:06:30 +0900

    #13 - 로그인 컨트롤러 테스트 + 시큐리티 설정을 컨트롤러 테스트에 반영
    스프링 시큐리티가 자동 생성해준 `/login` 컨트롤러를 테스트한다.
    그리고 모든 컨트롤러가 시큐리티 설정을 부를 수 있도록 `@Import` 를 추가하였다.

--- src/test/java/com/fastcampus/board/controller/ArticleControllerTest.java
+++ src/test/java/com/fastcampus/board/controller/ArticleControllerTest.java
@@ -1,10 +1,12 @@
 package com.fastcampus.board.controller;
 
+import com.fastcampus.board.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,6 +14,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 게시글")
+@Import(SecurityConfig.class)
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
     private final MockMvc mvc;
@@ -32,7 +35,7 @@ class ArticleControllerTest {
                 .andExpect(view().name("articles/index.html"))
                 .andExpect(model().attributeExists("articles"));
     }
-    
+
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
