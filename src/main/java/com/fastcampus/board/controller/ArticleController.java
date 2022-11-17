d5b6d4d624ff27615c5137f56549df7edb96dac7
eyxnz <todaytubuck@naver.com>
2022-11-17 15:58:02 +0900

    #13 - 게시글 뷰 기본 골격 작성
    컨트롤러, 뷰, 테스트까지 한 번에 적용했다.

--- src/main/java/com/fastcampus/board/controller/ArticleController.java
+++ src/main/java/com/fastcampus/board/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.fastcampus.board.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -16,5 +17,14 @@ public class ArticleController { // 게시글
         map.addAttribute("articles", List.of());
 
         return "articles/index.html";
+    }
+
+    // 게시글 상세 페이지
+    @GetMapping("/{articleId}")
+    public String article(@PathVariable Long articleId, ModelMap map) {
+        map.addAttribute("article", "article"); // TODO: 구현할 때 여기에 실제 데이터를 넣어줘야 한다
+        map.addAttribute("articleComments", List.of());
+
+        return "articles/detail";
     }
 }
