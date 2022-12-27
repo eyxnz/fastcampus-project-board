f5e790c2dba5dad8c8ca3097d449ac3635818002
eyxnz <todaytubuck@naver.com>
2022-12-27 18:42:23 +0900

    #21 - 페이지네이션 기능을 컨트롤러(게시판 뷰)에 반영
    테스트는 `Page` 클래스를 이용하기 때문에 하는 김에 페이징과 정렬을 함께 테스트하였다.
    나중에 정렬 구현에서 이어서 활용할 예정이다.

--- src/main/java/com/fastcampus/board/controller/ArticleController.java
+++ src/main/java/com/fastcampus/board/controller/ArticleController.java
@@ -4,7 +4,9 @@ import com.fastcampus.board.domain.type.SearchType;
 import com.fastcampus.board.dto.response.ArticleResponse;
 import com.fastcampus.board.dto.response.ArticleWithCommentsResponse;
 import com.fastcampus.board.service.ArticleService;
+import com.fastcampus.board.service.PaginationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RequestMapping("/articles")
 @Controller
@@ -22,6 +26,7 @@ public class ArticleController { // 게시글
     // 프론트에 보낼 땐 dto 가 아닌 response 로
 
     private final ArticleService articleService;
+    private final PaginationService paginationService;
 
     // 게시글 리스트 (게시판) 페이지
     @GetMapping
@@ -31,10 +36,13 @@ public class ArticleController { // 게시글
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable, // 한 페이지에 10개씩 생성일시 기준 내림차순
             ModelMap map
     ) {
-        map.addAttribute(
-                "articles",
-                articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from)
-        );
+        Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+        // pageable.getPageNumber() : 현재 페이지 정보
+        // articles.getTotalPages() : 전체 페이지 정보
+
+        map.addAttribute("articles", articles);
+        map.addAttribute("paginationBarNumbers", barNumbers);
 
         return "articles/index";
     }
